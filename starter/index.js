feda31031ad5039d6ffb93c75c0c9d5caf914651
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-02-28 10:46:09 +0000

    updated properties and methods for employee, manager, engineer and interm js files

--- starter/index.js
+++ starter/index.js
@@ -11,5 +11,19 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// TODO: Write Code to gather information about the development team members, 
+//and render the HTML file.
+
+function createEngineer(team){
+    inquirer.createPromptModule([
+        {
+        type: 'input',
+        name: 'name',
+        message: "What is the engineer's name?",
+        }
+        
+
+
+    ])
+}
 
--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require ('./Employee');
+
+class Engineer extends Employee {
+    constructor(name, id, email, gitHub) {
+        super (name, id, email);
+        gitHub.this = gitHub;
+    }
+
+    getGithub(){}
+    getRole(Engineer){}
+}
+
+module.exports = Engineer;
